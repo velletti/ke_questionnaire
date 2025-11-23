e249ba0697a5c97045d1886ffe2969885559327c
Joerg Velletti <jvelletti@allplan.com>
2025-11-23 13:07:00 +0100

    Remove dependencies, ranges and questions Types i do not use  and so can not test

--- Configuration/TCA/Overrides/tx_kequestionnaire_domain_model_dependancy.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-$GLOBALS['TCA']['tx_kequestionnaire_domain_model_dependancy']['ctrl']['security']['ignorePageTypeRestriction'] = true;
--- Configuration/TCA/Overrides/tx_kequestionnaire_domain_model_range.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-$GLOBALS['TCA']['tx_kequestionnaire_domain_model_range']['ctrl']['security']['ignorePageTypeRestriction'] = true;
